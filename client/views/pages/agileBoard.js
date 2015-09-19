dc18a28873743dc4df1a85de15c9023e6687e9a3
Liu Jiyang <2411mail@gmail.com>
2015-09-20 01:57:56 +0800

    connect agile board with time board

--- client/views/pages/agileBoard.js
+++ client/views/pages/agileBoard.js
@@ -9,15 +9,42 @@ Template.agileBoard.rendered = function(){
 Template.agileBoard.helpers({
   tasks: function(){
     return Tasks.find();
+  },
+  status: function(){
+    return this.status || "new";
+  },
+  startDate: function() {
+    var d = new Date(this.start_date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
   }
 });
 
 Template.agileBoard.events({
   "click #new-task-submit": function(event, template){
-    var title = template.$("#new-task-input").val();
-    var now = new Date();
+    var id = Random.id([13]);
+    var start_date = new Date();
+    var text = template.$("#new-task-input").val();
+    var duration = 1;
     var status = "new";
-    console.log(title);
-    Tasks.insert({title: title, due: now, status: status});
+    var end_date = new Date();
+    end_date.setDate(end_date.getDate() + 1);
+    var parent = 0;
+    var taskObject = {
+      id: id,
+      start_date: start_date,
+      text: text,
+      duration: duration,
+      status: status,
+      end_date: end_date,
+      parent: parent
+    };
+    Tasks.insert(taskObject);
   }
 });
