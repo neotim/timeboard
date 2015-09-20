bc860f83bbeb8a5647789a8bcaf918bc2b16012c
Liu Jiyang <2411mail@gmail.com>
2015-09-20 14:33:30 +0800

    change scale

--- client/views/pages/timeBoard.js
+++ client/views/pages/timeBoard.js
@@ -1,6 +1,7 @@
 Template.timeBoard.rendered = function(){
   //Init dhtmlxGantt.
-  gantt.init("task-gantt");
+  gantt.init("task-gantt", new Date(), new Date(2015, 11, 22));
+
 
   //Init dhtmlxGantt data adapter.
   //gantt.meteor({tasks: TasksCollection, links: LinksCollection});
